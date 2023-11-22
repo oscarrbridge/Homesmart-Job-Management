35e1a77eeb083be0eb788e278bae314fee2a2368
Oscar Bridge <oscarbridge.nz@gmail.com>
2023-11-22 15:38:45 +1300

    Login Class

---
+++ Homesmart-Job-Management/login.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homesmart_Job_Management
+{
+    public static class DbConfig
+    {
+        public const string Server = "shieldcoat.ddns.net";
+        public const string Port = "3306";
+        public const string Database = "homesmart-job-database";
+        public const string Uid = "kk";
+        public const string Password = "shieldCOAT!";
+    }
+}
